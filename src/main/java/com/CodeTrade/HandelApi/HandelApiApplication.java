a5c55fd7273c462ed010ae36d9c494cf604b6e52
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-10-31 11:11:19 -0500

    cors

--- src/main/java/com/CodeTrade/HandelApi/HandelApiApplication.java
+++ src/main/java/com/CodeTrade/HandelApi/HandelApiApplication.java
@@ -2,8 +2,11 @@ package com.CodeTrade.HandelApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -12,4 +15,15 @@ public class HandelApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HandelApiApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("/*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
+
