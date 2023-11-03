d69ba95939b18071ef7614cb691ad78e64656154
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-11-03 14:39:37 -0500

    cambio a long text en objeto trueque

--- src/main/java/com/CodeTrade/HandelApi/persistence/entity/ObjetoTrueque.java
+++ src/main/java/com/CodeTrade/HandelApi/persistence/entity/ObjetoTrueque.java
@@ -23,8 +23,8 @@ public class ObjetoTrueque {
     @Column(name="idtrueques", nullable = false, unique = true)
     private Integer idTrueques;
     @Lob
-    @Column(columnDefinition = "MEDIUMBLOB")
-    private byte[] imagen;
+//    @Column(columnDefinition = "MEDIUMBLOB")
+    private String imagen;
     @Column(length = 50,nullable = false)
     private String  titulo;
     @Column(length = 255,nullable = false)
